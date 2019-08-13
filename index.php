1960405494bdd2bb26987222f5dbf0ac9ce2c73f
japjanfu <jap.janfu.ukrida@gmail.com>
2019-08-14 02:57:05 +0700

    menambahkan liat pada file index.php

--- index.php
+++ index.php
@@ -9,5 +9,10 @@
 <body>
     <h1>hello world</h1>
     <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sint aperiam dolorum tempora distinctio atque iure fugiat inventore! Temporibus libero veritatis nulla at, eius cupiditate aperiam, culpa hic rerum nesciunt laborum?</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+        <li>tiga</li>
+    </ul>
 </body>
 </html>
