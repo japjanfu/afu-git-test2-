60d6b66461dc87e11084a4456c2793a8186b0d52
japjanfu <jap.janfu.ukrida@gmail.com>
2019-08-14 02:46:26 +0700

    menambahkan paragraph

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Coba Git Remote</title>
+</head>
+<body>
+    <h1>hello world</h1>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sint aperiam dolorum tempora distinctio atque iure fugiat inventore! Temporibus libero veritatis nulla at, eius cupiditate aperiam, culpa hic rerum nesciunt laborum?</p>
+</body>
+</html>
